52144435f931f2cf7d91c0d6a52886bb099db666
Chebyrek_18 <119298807+Chebyrek-18@users.noreply.github.com>
2025-02-12 09:19:53 +0300

    Delete incl directory

--- incl/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="container">
-    <img src="/assets/image/logo-home.png" alt="Лого" class="logo">
-    <div class="contacts">
-        <a href="tel:+79376110288">+7 (937) 611-02-88</a>
-        <!-- <a href="mailto:rustemhadiev2006@mail.ru"><img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-email-512.png" alt=""></a> -->
-        <!-- <a href="https://t.me/Chebyrek_18"><img src="https://download.logo.wine/logo/Telegram_(software)/Telegram_(software)-Logo.wine.png" alt=""></a> -->
-    </div>
-</footer>
--- incl/home.php
+++
@@ -1,5 +0,0 @@
-<main class="container">
-    <div class="banner">
-        <img src="/assets/image/banner.png" alt="">
-    </div>
-</main>
